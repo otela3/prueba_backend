79e95d9d9ff5bd7d3f12199be081a6ee34dfeb6a
otela3 <motelo@fabricadecodigo.cl>
2021-12-25 12:43:57 -0800

    uvicorn en docker-compose

--- database/database.py
+++ database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import orm
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:password@localhost:5432/pruebadb"
+SQLALCHEMY_DATABASE_URL = "postgresql://myuser:password@postgres/pruebadb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
